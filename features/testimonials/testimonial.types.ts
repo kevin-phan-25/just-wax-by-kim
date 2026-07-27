96b412292861aee8539f5eba9867865c72228c66
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:04:32 -0400

    Update testimonial.types.ts

---
+++ features/testimonials/testimonial.types.ts
@@ -0,0 +1,33 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: testimonial.types.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Type definitions for customer testimonials.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial testimonial architecture.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export interface Testimonial {
+
+
+    id: string;
+
+
+    name: string;
+
+
+    quote: string;
+
+
+    rating: number;
+
+
+}
