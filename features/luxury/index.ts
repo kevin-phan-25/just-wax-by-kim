d06a0112dcf42bfa0503755ebb40d434d4f32d0d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:32:28 -0400

    Create index.ts

---
+++ features/luxury/index.ts
@@ -0,0 +1,18 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: index.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Luxury feature exports.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added v1.0.2 luxury visual experience.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export { default } from "./LuxuryPromise";
