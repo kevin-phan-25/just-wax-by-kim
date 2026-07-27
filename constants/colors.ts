a568390e4b425d936a427e9353cb03dca83fc898
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:05:19 -0400

    Add brand color constants in colors.ts
    Added brand color constants for use in the application.

---
+++ constants/colors.ts
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: constants/colors.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Brand color constants.
+ *
+ * Changes:
+ * - v1.0.2
+ *   - Centralized luxury palette.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const COLORS = {
+
+
+ gold:"#C9A227",
+
+ champagne:"#F5E6C8",
+
+ cream:"#FAF7F2",
+
+ ivory:"#FFFDF8",
+
+ espresso:"#3B2A24",
+
+ rose:"#D8A7A7",
+
+ black:"#111111"
+
+
+};
