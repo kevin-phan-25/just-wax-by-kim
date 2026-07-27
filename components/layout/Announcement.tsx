6a3ebb7793df94d61202e0b3cc57fccaf4002acf
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:19:32 -0400

    Update Announcement.tsx

---
+++ components/layout/Announcement.tsx
@@ -0,0 +1,52 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: Announcement.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Top announcement banner for promotions, booking notices,
+ * and important customer messaging.
+ *
+ * Changes:
+ *
+ * - July 27, 2026
+ *   - Added v1.1.0 luxury layout foundation.
+ *   - Added reusable announcement component.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function Announcement(){
+
+
+return (
+
+<div
+className="
+w-full
+bg-brand-dark
+text-brand-gold
+text-sm
+tracking-wide
+text-center
+py-3
+px-4
+"
+>
+
+
+<p>
+
+Now Booking Luxury Wax Appointments
+
+</p>
+
+
+</div>
+
+);
+
+
+}
