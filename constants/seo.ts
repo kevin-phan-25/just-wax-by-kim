7a50fee15e0061d882399d300645a5725d0dbb56
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:12:42 -0400

    Update seo.ts

---
+++ constants/seo.ts
@@ -0,0 +1,38 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: seo.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * SEO keyword configuration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added production SEO keywords.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const seoConfig = {
+
+
+keywords:[
+
+"waxing studio",
+
+"luxury waxing",
+
+"professional waxing",
+
+"Brazilian waxing",
+
+"Virginia waxing",
+
+"Just Wax by Kim"
+
+]
+
+
+};
