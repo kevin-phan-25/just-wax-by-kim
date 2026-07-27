1d45126e610a7d353205d257ad65d8f8fa0819f9
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:50:30 -0400

    Update about.constants.ts

---
+++ features/about/about.constants.ts
@@ -0,0 +1,23 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: about.constants.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Static configuration for About section.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const ABOUT_IMAGE = {
+
+  placeholder:
+    "/about/kim-placeholder.svg",
+
+};
