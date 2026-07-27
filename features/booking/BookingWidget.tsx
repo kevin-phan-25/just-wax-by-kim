a4e9d853925357a0e87b590478c54dbc0fb0f00a
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:58:05 -0400

    Update BookingWidget.tsx

---
+++ features/booking/BookingWidget.tsx
@@ -0,0 +1,61 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: BookingWidget.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Future booking platform integration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added booking provider placeholder.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function BookingWidget(){
+
+
+return (
+
+<div
+
+className="
+rounded-3xl
+bg-brand-white
+p-10
+text-center
+shadow-sm
+"
+
+>
+
+
+<h3 className="
+text-2xl
+">
+
+Online Booking
+
+</h3>
+
+
+
+<p className="
+mt-4
+text-brand-taupe
+">
+
+Booking integration coming soon.
+
+</p>
+
+
+
+</div>
+
+);
+
+}
