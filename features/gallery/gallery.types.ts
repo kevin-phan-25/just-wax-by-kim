7df4ae3be070535b6f7407efd37cf894d21ee97f
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:01:04 -0400

    Update gallery.types.ts

---
+++ features/gallery/gallery.types.ts
@@ -0,0 +1,33 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: gallery.types.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Type definitions for gallery feature.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial gallery types.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export interface GalleryImage {
+
+
+    id: string;
+
+
+    src: string;
+
+
+    alt: string;
+
+
+    title: string;
+
+
+}
