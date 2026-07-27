5da24f373bfb004b5e64d65e4005aeb916fd9a69
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:25:52 -0400

    Create seo.ts

---
+++ config/seo.ts
@@ -0,0 +1,51 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: seo.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * SEO configuration for Just Wax by Kim.
+ *
+ * This file centralizes:
+ * - Search keywords
+ * - Brand SEO phrases
+ * - Local search terms
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added v1.0.0 production SEO configuration.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const seoConfig = {
+
+
+keywords: [
+
+"Just Wax by Kim",
+
+"luxury waxing studio",
+
+"professional waxing services",
+
+"Brazilian wax",
+
+"bikini wax",
+
+"facial waxing",
+
+"body waxing",
+
+"Virginia waxing studio",
+
+"luxury beauty services",
+
+"wax specialist"
+
+]
+
+
+};
