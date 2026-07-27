5fc75e31b7950db17bb22342c74c0e5fe7d03b78
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:20:44 -0400

    Update postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,39 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: postcss.config.mjs
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * PostCSS configuration.
+ *
+ * Enables:
+ *
+ * - Tailwind CSS processing
+ * - Production CSS optimization
+ *
+ * Changes:
+ *
+ * - July 27, 2026
+ *   - Added v1.1.0 Tailwind v4 support.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+const config = {
+
+
+plugins:{
+
+
+"@tailwindcss/postcss": {},
+
+
+},
+
+
+};
+
+
+export default config;
