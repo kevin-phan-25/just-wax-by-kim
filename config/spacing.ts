7324ef3ca96efd194dcaacd0fead2a4acbf78867
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:35:33 -0400

    Create spacing.ts

---
+++ config/spacing.ts
@@ -0,0 +1,32 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: spacing.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Global spacing scale.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial luxury spacing system.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const spacing = {
+
+  section:
+    "6rem",
+
+  sectionMobile:
+    "4rem",
+
+  container:
+    "80rem",
+
+  card:
+    "1.5rem",
+
+} as const;
