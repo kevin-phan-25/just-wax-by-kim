19b516649a8d0d29c51b32c68d4473523153c158
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:51:56 -0400

    Update Mission.tsx

---
+++ features/about/Mission.tsx
@@ -0,0 +1,47 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: Mission.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Brand mission component.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function Mission() {
+
+
+return (
+
+<div
+className="
+border-l
+border-brand-champagne
+pl-6
+"
+>
+
+
+<p className="
+italic
+text-brand-taupe
+">
+
+"Every appointment should leave you feeling
+confident, comfortable, and beautiful."
+
+</p>
+
+
+</div>
+
+);
+
+}
