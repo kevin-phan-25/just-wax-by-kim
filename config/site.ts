eff99cb3c5e3daf4adc0723926208a74a74dea04
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:10:16 -0400

    Update site.ts

---
+++ config/site.ts
@@ -0,0 +1,39 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: site.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Global website configuration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+export const siteConfig = {
+
+  name:
+    "Just Wax by Kim",
+
+  slogan:
+    "More Than Just a Wax",
+
+  description:
+    "Luxury waxing services designed around confidence, comfort, and beauty.",
+
+  url:
+    "https://justwaxbykim.com",
+
+  keywords: [
+    "waxing",
+    "luxury waxing",
+    "Brazilian waxing",
+    "beauty studio",
+    "wax studio",
+  ],
+
+};
