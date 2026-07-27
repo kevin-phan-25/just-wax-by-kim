eb004e8ca3ac63e8783ada3381cb71488f495f2b
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:05:38 -0400

    Create instagram.constants.ts with configuration
    Add Instagram configuration constants for handle, title, and description.

---
+++ features/instagram/instagram.constants.ts
@@ -0,0 +1,35 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: instagram.constants.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Instagram configuration.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const INSTAGRAM_CONFIG = {
+
+
+handle:
+
+"@justwaxbykim",
+
+
+
+title:
+
+"Follow Our Journey",
+
+
+
+description:
+
+"See behind the scenes, studio moments, and beautiful results."
+
+
+
+};
