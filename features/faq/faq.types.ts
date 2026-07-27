7cf718910f2a72ae49b91654eada90f90667234e
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:07:45 -0400

    Update faq.types.ts

---
+++ features/faq/faq.types.ts
@@ -0,0 +1,26 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: faq.types.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * FAQ type definitions.
+ *
+ * Changes:
+ * - v1.0.2
+ *   - Added reusable FAQ interfaces.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export interface FAQItemType {
+
+    id:string;
+
+    question:string;
+
+    answer:string;
+
+}
