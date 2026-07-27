646e3a125d7334e3814d40202c6dfb8ea7f19169
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:09:08 -0400

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,75 +5,128 @@
  * Created: July 27, 2026
  *
  * Description:
- * Main website navigation.
+ * Main luxury navigation.
  *
  * Changes:
  * - July 27, 2026
- *   - Initial desktop navigation.
- *   - Added primary booking CTA.
+ *   - Added v0.9.0 navbar.
  *
  * -----------------------------------------------------------------------------
  */
 
 
-import Container from "@/components/ui/Container";
-import Button from "@/components/ui/Button";
+import Link from "next/link";
 
 
-export default function Navbar() {
+import {
+
+navigation
+
+}
+
+from "@/constants/navigation";
+
+
+import Logo from "../ui/Logo";
+
+
+import MobileMenu from "./MobileMenu";
+
+
+
+export default function Navbar(){
 
 
 return (
 
-<nav className="
-fixed
+<header
+
+className="
+sticky
 top-0
 z-50
-w-full
 border-b
-border-[#ECE7E1]
-bg-[#FAF8F5]/90
+border-brand-border
+bg-brand-white/90
 backdrop-blur
-">
+"
+
+>
 
 
-<Container>
+<div
 
-
-<div className="
+className="
+mx-auto
 flex
-h-20
+max-w-7xl
 items-center
 justify-between
-">
+px-6
+py-5
+"
+
+>
 
 
-<div className="
-text-2xl
-font-semibold
+<Logo />
+
+
+
+<nav
+
+className="
+hidden
+md:flex
+gap-8
+"
+
+>
+
+
+{
+
+navigation.map(item=>(
+
+
+<Link
+
+key={item.label}
+
+href={item.href}
+
+className="
+text-sm
 tracking-wide
-">
+hover:text-brand-gold
+"
 
-Just Wax by Kim
+>
 
-</div>
+{item.label}
 
-
-<Button>
-
-Book Now
-
-</Button>
+</Link>
 
 
-</div>
+))
 
 
-</Container>
+}
 
 
 </nav>
 
+
+
+<MobileMenu />
+
+
+</div>
+
+
+</header>
+
 );
+
 
 }
