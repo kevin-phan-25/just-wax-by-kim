7ec14bc229f5410375428fa03ba3f93384617cae
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:10:44 -0400

    Add initial business configuration
    Initial configuration for business information including name, slogan, and contact details.

---
+++ constants/business.ts
@@ -0,0 +1,38 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: business.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Business information used throughout the website.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial business configuration.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const business = {
+
+  name:
+    "Just Wax by Kim",
+
+  slogan:
+    "More Than Just a Wax",
+
+  phone:
+    "",
+
+  email:
+    "",
+
+  address:
+    "",
+
+  instagram:
+    "",
+
+};
