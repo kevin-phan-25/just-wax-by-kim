bf71375661338c51f49ef146f0ddfdf14d4f9a14
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:57:16 -0400

    Update booking.types.ts

---
+++ features/booking/booking.types.ts
@@ -0,0 +1,34 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: booking.types.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Type definitions for booking feature.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export interface BookingBenefit {
+
+    title: string;
+
+    description: string;
+
+}
+
+
+
+export interface BusinessHour {
+
+    day: string;
+
+    hours: string;
+
+}
