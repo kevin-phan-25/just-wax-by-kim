3cd642757f49e956ee2f919e1c7585931b0ae390
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:13:56 -0400

    Update not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,60 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: not-found.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Custom 404 page.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function NotFound() {
+
+
+return (
+
+<div className="
+flex
+min-h-screen
+items-center
+justify-center
+">
+
+
+<div className="text-center">
+
+
+<h1 className="
+text-6xl
+">
+
+404
+
+</h1>
+
+
+<p className="
+mt-4
+text-[#8A817C]
+">
+
+This page could not be found.
+
+</p>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
