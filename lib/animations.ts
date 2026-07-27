bf0d284269a8ffbfa43fad21b53621beb3b08ecd
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:36:46 -0400

    Update animations.ts

---
+++ lib/animations.ts
@@ -0,0 +1,49 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: animations.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Framer Motion animation presets.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added luxury reveal animations.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const fadeUp = {
+
+initial: {
+
+opacity:
+0,
+
+y:
+30,
+
+},
+
+
+animate: {
+
+opacity:
+1,
+
+y:
+0,
+
+},
+
+
+transition: {
+
+duration:
+0.7,
+
+},
+
+};
