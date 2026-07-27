f1a59fbccdece60c61f0717a048fecf63a396691
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:42:42 -0400

    Update About.tsx

---
+++ features/about/About.tsx
@@ -0,0 +1,56 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: About.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * About section placeholder.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added homepage section placeholder.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function About() {
+
+
+return (
+
+<section className="
+px-6
+py-20
+text-center
+">
+
+
+<h2 className="
+text-4xl
+">
+
+Meet Kim
+
+</h2>
+
+
+<p className="
+mx-auto
+mt-6
+max-w-xl
+text-brand-taupe
+">
+
+Creating a comfortable and elevated waxing
+experience designed around confidence.
+
+</p>
+
+
+</section>
+
+);
+
+}
