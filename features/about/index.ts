f95dca223fa91102e99b2d4ebc4be3d00cc853e2
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:46:59 -0400

    Update index.ts

---
+++ features/about/index.ts
@@ -0,0 +1,20 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: index.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * About feature public exports.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added About section export.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export {
+default
+} from "./About";
