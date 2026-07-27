9f0ceae135de950234e3430b391431eea7d3fefa
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:06:08 -0400

    Update FollowButton.tsx

---
+++ features/instagram/FollowButton.tsx
@@ -0,0 +1,41 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: FollowButton.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Instagram CTA button.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function FollowButton(){
+
+
+return (
+
+<a
+
+href="#"
+
+className="
+inline-flex
+rounded-full
+bg-brand-gold
+px-8
+py-4
+text-white
+"
+
+>
+
+Follow Instagram
+
+</a>
+
+);
+
+
+}
