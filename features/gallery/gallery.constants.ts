eda94e3c0c501940c2a848efa486cf06b4285735
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:01:13 -0400

    Update gallery.constants.ts

---
+++ features/gallery/gallery.constants.ts
@@ -0,0 +1,38 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: gallery.constants.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Gallery configuration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added gallery settings.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const GALLERY_CONFIG = {
+
+
+    eyebrow:
+
+    "Our Studio",
+
+
+
+    title:
+
+    "A Luxury Experience From Start To Finish",
+
+
+
+    description:
+
+    "Explore the atmosphere, attention to detail, and care behind every appointment."
+
+
+};
