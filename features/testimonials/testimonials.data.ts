bde19caf1d7effe8ac03d1a7c4d31515a8574697
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:04:44 -0400

    Update testimonials.data.ts

---
+++ features/testimonials/testimonials.data.ts
@@ -0,0 +1,97 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: testimonials.data.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Customer testimonial data.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added initial customer reviews.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+import type {
+
+Testimonial
+
+}
+
+from "./testimonial.types";
+
+
+
+export const testimonials: Testimonial[] = [
+
+
+{
+
+id:
+
+"1",
+
+name:
+
+"Samantha",
+
+quote:
+
+"Kim made my first waxing experience comfortable and relaxing. The attention to detail was incredible.",
+
+rating:
+
+5
+
+},
+
+
+
+{
+
+id:
+
+"2",
+
+name:
+
+"Jessica",
+
+quote:
+
+"The studio feels luxurious and welcoming. Kim truly cares about her clients.",
+
+rating:
+
+5
+
+},
+
+
+
+{
+
+id:
+
+"3",
+
+name:
+
+"Emily",
+
+quote:
+
+"Professional, gentle, and amazing results every appointment.",
+
+rating:
+
+5
+
+}
+
+
+
+];
