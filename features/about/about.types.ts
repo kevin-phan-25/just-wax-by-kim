af2211dc198e32cd1524319ca5b7f45b0fc02ddf
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:50:20 -0400

    Update about.types.ts

---
+++ features/about/about.types.ts
@@ -0,0 +1,35 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: about.types.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Type definitions for About Kim feature.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export interface Credential {
+
+  title: string;
+
+  description: string;
+
+}
+
+
+export interface AboutSection {
+
+  eyebrow: string;
+
+  title: string;
+
+  description: string;
+
+}
