1c71fdf40abd0b116dafd449e59e8fb848a70df3
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:08:39 -0400

    Update social.ts

---
+++ constants/social.ts
@@ -0,0 +1,47 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: social.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Social media configuration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added social links.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const socialLinks = [
+
+
+{
+
+name:
+
+"Instagram",
+
+url:
+
+"https://instagram.com/justwaxbykim"
+
+},
+
+
+{
+
+name:
+
+"Facebook",
+
+url:
+
+"https://facebook.com"
+
+}
+
+
+];
