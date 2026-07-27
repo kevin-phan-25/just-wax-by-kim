ce47b6c3696bfc22e37e066aa641b0d05a66f0d3
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:37:26 -0400

    Update Input.tsx

---
+++ components/ui/Input.tsx
@@ -0,0 +1,32 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: Divider.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Decorative luxury divider.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial creation.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function Divider() {
+
+
+return (
+
+<div className="
+mx-auto
+h-px
+w-24
+bg-brand-champagne
+"/>
+
+);
+
+}
