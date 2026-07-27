2e4a013d94db71f67b0762cfe5fa88f81ef7f641
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:09:31 -0400

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,51 +5,215 @@
  * Created: July 27, 2026
  *
  * Description:
- * Website footer.
+ * Luxury footer section.
  *
  * Changes:
  * - July 27, 2026
- *   - Initial creation.
+ *   - Added v0.9.0 footer redesign.
  *
  * -----------------------------------------------------------------------------
  */
 
 
-import Container from "@/components/ui/Container";
+import {
+
+business
+
+}
+
+from "@/constants/business";
 
 
-export default function Footer() {
+import {
+
+socialLinks
+
+}
+
+from "@/constants/social";
+
+
+
+export default function Footer(){
 
 
 return (
 
-<footer className="
-border-t
-border-[#ECE7E1]
-py-10
-">
+<footer
+
+className="
+bg-brand-black
+px-6
+py-16
+text-white
+"
+
+>
 
 
-<Container>
+<div
+
+className="
+mx-auto
+max-w-7xl
+grid
+gap-10
+md:grid-cols-3
+"
+
+>
+
+
+<div>
+
+
+<h2
+
+className="
+text-2xl
+"
+
+>
+
+{business.name}
+
+</h2>
 
 
 <p className="
-text-center
-text-sm
-text-[#8A817C]
-">
+mt-4
+text-white/70
+"
 
-© {new Date().getFullYear()} Just Wax by Kim.
-All rights reserved.
+>
+
+{business.tagline}
 
 </p>
 
 
-</Container>
+</div>
+
+
+
+
+<div>
+
+
+<h3>
+
+Connect
+
+</h3>
+
+
+<div className="
+mt-4
+space-y-3
+"
+
+>
+
+
+{
+
+socialLinks.map(item=>(
+
+
+<a
+
+key={item.name}
+
+href={item.url}
+
+className="
+block
+text-white/70
+hover:text-white
+"
+
+>
+
+{item.name}
+
+</a>
+
+
+))
+
+
+}
+
+
+</div>
+
+
+</div>
+
+
+
+
+<div>
+
+
+<h3>
+
+Contact
+
+</h3>
+
+
+<p className="
+mt-4
+text-white/70
+"
+
+>
+
+{business.location}
+
+</p>
+
+
+<p>
+
+{business.phone}
+
+</p>
+
+
+</div>
+
+
+</div>
+
+
+
+<div
+
+className="
+mt-12
+border-t
+border-white/20
+pt-6
+text-center
+text-sm
+text-white/60
+"
+
+>
+
+
+© 2026 {business.name}. All rights reserved.
+
+
+</div>
+
 
 
 </footer>
 
 );
+
 
 }
