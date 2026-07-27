8bebf0fe8da1c2a871410c1adc5d331c8ae0e56e
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:14:37 -0400

    Update contact.data.ts

---
+++ features/contact/contact.data.ts
@@ -0,0 +1,52 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: contact.data.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Contact business information.
+ *
+ * Changes:
+ * - v1.0.3
+ *   - Added business contact configuration.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const CONTACT_DATA = {
+
+
+    businessName:
+    "Just Wax by Kim",
+
+
+    phone:
+    "(555) 555-5555",
+
+
+    email:
+    "hello@justwaxbykim.com",
+
+
+    location:
+    "Luxury Wax Studio",
+
+
+    address:
+    "Virginia, United States",
+
+
+
+    instagram:
+    "@justwaxbykim",
+
+
+
+    description:
+    "Questions about services, appointments, or your first waxing experience? Kim is here to help."
+
+
+
+};
