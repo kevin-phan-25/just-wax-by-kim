54019a5b7a72c5d71880cd68efba9d9e9dbcc4e3
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:55:03 -0400

    Update ServicePricing.tsx

---
+++ features/services/ServicePricing.tsx
@@ -0,0 +1,36 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: ServicePricing.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Pricing introduction component.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added pricing messaging.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function ServicePricing(){
+
+
+return (
+
+<p className="
+mt-8
+text-sm
+text-brand-taupe
+">
+
+Pricing may vary based on customization.
+Consultation available before your appointment.
+
+</p>
+
+);
+
+}
