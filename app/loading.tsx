cff872c18a0d307dd601c5bb732d99adfa30bcf6
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:13:45 -0400

    Create Loading component for route transitions
    Initial implementation of a loading screen component for route transitions.

---
+++ app/loading.tsx
@@ -0,0 +1,44 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: loading.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Loading UI displayed during route transitions.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial loading screen.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function Loading() {
+
+
+return (
+
+<div className="
+flex
+min-h-screen
+items-center
+justify-center
+">
+
+
+<p className="
+text-[#C9A227]
+">
+
+Loading...
+
+</p>
+
+
+</div>
+
+);
+
+}
