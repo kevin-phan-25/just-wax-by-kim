0e9a7b8d56a385828bcec637e2a62e5723cf810d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:54:15 -0400

    Update services.constants.ts

---
+++ features/services/services.constants.ts
@@ -0,0 +1,31 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: services.constants.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Service configuration constants.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added service section constants.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const SERVICES_CONFIG = {
+
+    eyebrow:
+    "Our Services",
+
+
+    title:
+    "Luxury Waxing Designed Around You",
+
+
+    description:
+    "Every service is performed with attention to comfort, quality, and confidence."
+
+};
