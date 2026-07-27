e97c1edcc820b358739d46c221cf278124f1af53
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:35:54 -0400

    Create typography.ts for typography scale
    Added a typography scale with heading and body styles.

---
+++ config/typography.ts
@@ -0,0 +1,46 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: typography.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Typography scale.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added luxury heading/body hierarchy.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const typography = {
+
+
+heading: {
+
+  hero:
+    "text-6xl md:text-8xl",
+
+  section:
+    "text-4xl md:text-5xl",
+
+},
+
+
+body: {
+
+  large:
+    "text-lg",
+
+  normal:
+    "text-base",
+
+  small:
+    "text-sm",
+
+},
+
+
+};
