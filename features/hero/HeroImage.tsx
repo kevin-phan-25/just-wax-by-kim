11851e1070d3c6683ba6043b69f1aff55e1ef24b
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:42:20 -0400

    Update HeroImage.tsx

---
+++ features/hero/HeroImage.tsx
@@ -0,0 +1,58 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: HeroImage.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Hero image container.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added luxury image placeholder.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function HeroImage() {
+
+
+return (
+
+<div className="
+relative
+overflow-hidden
+rounded-[2rem]
+">
+
+
+<div className="
+aspect-[4/5]
+bg-brand-rose/20
+flex
+items-center
+justify-center
+">
+
+
+<p className="
+text-center
+text-brand-taupe
+">
+
+Luxury Studio
+<br/>
+Image Coming Soon
+
+</p>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
