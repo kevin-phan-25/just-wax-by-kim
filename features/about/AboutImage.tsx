7431cfcf9579f7d3a81d30a55c63f4cb614a0fe1
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:51:38 -0400

    Update AboutImage.tsx

---
+++ features/about/AboutImage.tsx
@@ -0,0 +1,60 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: AboutImage.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Kim profile image area.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added luxury placeholder.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function AboutImage() {
+
+
+return (
+
+<div
+className="
+overflow-hidden
+rounded-[2rem]
+"
+>
+
+
+<div
+className="
+aspect-[4/5]
+bg-brand-rose/20
+flex
+items-center
+justify-center
+"
+>
+
+
+<p className="
+text-brand-taupe
+">
+
+Kim's Photo
+<br/>
+Coming Soon
+
+</p>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
