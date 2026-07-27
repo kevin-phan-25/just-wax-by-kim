c7bb5e1013b1720dbd1428b56cb34b3e837a7ac6
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 11:37:00 -0400

    Update Logo.tsx

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,54 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: Logo.tsx
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Luxury brand logo component.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Initial logo system.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export default function Logo() {
+
+
+return (
+
+<div className="
+text-center
+leading-none
+">
+
+
+<p className="
+text-2xl
+tracking-[0.3em]
+">
+
+JUST WAX
+
+</p>
+
+
+<p className="
+text-sm
+tracking-[0.5em]
+text-brand-champagne
+">
+
+BY KIM
+
+</p>
+
+
+</div>
+
+);
+
+}
