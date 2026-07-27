f7dddbc7f6698fe950b9565cc671e99d601412d1
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:08:42 -0400

    Update index.ts

---
+++ features/faq/index.ts
@@ -0,0 +1,21 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: index.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * FAQ feature exports.
+ *
+ * Changes:
+ * - v1.0.2
+ *   - Added public FAQ module export.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export {
+default
+}
+from "./FAQ";
