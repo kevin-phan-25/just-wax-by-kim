8799c02336c20512ffd6d5b40fdaf29cf1cf239f
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:05:45 -0400

    Update instagram.data.ts

---
+++ features/instagram/instagram.data.ts
@@ -0,0 +1,75 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: instagram.data.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Instagram placeholder content.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added social feed architecture.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+import type {
+
+InstagramPost
+
+}
+
+from "./instagram.types";
+
+
+
+export const instagramPosts: InstagramPost[] = [
+
+
+{
+
+id:"1",
+
+image:"/gallery/studio-1.webp",
+
+alt:"Studio"
+
+},
+
+
+{
+
+id:"2",
+
+image:"/gallery/studio-2.webp",
+
+alt:"Treatment room"
+
+},
+
+
+{
+
+id:"3",
+
+image:"/gallery/studio-3.webp",
+
+alt:"Luxury details"
+
+},
+
+
+{
+
+id:"4",
+
+image:"/gallery/luxury-detail.webp",
+
+alt:"Products"
+
+}
+
+
+];
