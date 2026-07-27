10a44ecdae6a53450b3eb72fd83938d5ebd08617
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 12:08:28 -0400

    Update navigation.ts

---
+++ constants/navigation.ts
@@ -0,0 +1,93 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: navigation.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Main website navigation configuration.
+ *
+ * Changes:
+ * - July 27, 2026
+ *   - Added v0.9.0 navigation system.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const navigation = [
+
+
+{
+label:
+
+"Home",
+
+href:
+
+"#home"
+
+},
+
+
+{
+label:
+
+"About",
+
+href:
+
+"#about"
+
+},
+
+
+{
+label:
+
+"Services",
+
+href:
+
+"#services"
+
+},
+
+
+{
+label:
+
+"Gallery",
+
+href:
+
+"#gallery"
+
+},
+
+
+{
+label:
+
+"Reviews",
+
+href:
+
+"#testimonials"
+
+},
+
+
+{
+label:
+
+"Book",
+
+href:
+
+"#booking"
+
+}
+
+
+];
