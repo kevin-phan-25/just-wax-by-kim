9ac725a35e2269612ee2530249529ca001a6606d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:07:52 -0400

    Update faq.constants.ts

---
+++ features/faq/faq.constants.ts
@@ -0,0 +1,34 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: faq.constants.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * FAQ configuration values.
+ *
+ * Changes:
+ * - v1.0.2
+ *   - Added FAQ section metadata.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export const FAQ_CONSTANTS = {
+
+
+    title:
+        "Frequently Asked Questions",
+
+
+    subtitle:
+        "Everything you need to know before your luxury waxing experience.",
+
+
+
+    sectionId:
+        "faq"
+
+
+};
