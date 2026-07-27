847a8b475cfdbefb11676311c1b9cd149ef40ad9
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-27 13:12:49 -0400

    Update index.ts

---
+++ features/experience/index.ts
@@ -0,0 +1,24 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File: index.ts
+ *
+ * Created: July 27, 2026
+ *
+ * Description:
+ * Experience module export.
+ *
+ * Changes:
+ * - v1.0.3
+ *   - Added public export.
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+
+export {
+
+default
+
+}
+
+from "./Experience";
