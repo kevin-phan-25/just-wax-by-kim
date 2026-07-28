efa9a9f455afcabd64aee22bb86ca032b678f0a7
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 06:06:01 -0400

    Update About.tsx

--- features/about/About.tsx
+++ features/about/About.tsx
@@ -1,68 +1,27 @@
 /**
  * -----------------------------------------------------------------------------
- * File:
- * About.tsx
+ * File: About.tsx
  *
  * Changes:
- * - v1.0.1 luxury about redesign.
+ * - July 28, 2026
+ *   - Cleaner luxury layout
+ *   - Better spacing and hierarchy
  *
  * -----------------------------------------------------------------------------
  */
-
-
 import AboutContent from "./AboutContent";
-
 import AboutImage from "./AboutImage";
 
-
-
-export default function About(){
-
-
-return (
-
-
-<section
-
-id="about"
-
-className="
-bg-white
-px-6
-py-24
-"
-
-
->
-
-
-<div
-
-className="
-mx-auto
-max-w-6xl
-grid
-md:grid-cols-2
-gap-16
-items-center
-"
-
-
->
-
-
-<AboutImage />
-
-<AboutContent />
-
-
-</div>
-
-
-</section>
-
-
-);
-
-
+export default function About() {
+  return (
+    <section
+      id="about"
+      className="bg-white px-6 py-24 md:py-32"
+    >
+      <div className="mx-auto max-w-6xl grid md:grid-cols-2 gap-12 md:gap-20 items-center">
+        <AboutImage />
+        <AboutContent />
+      </div>
+    </section>
+  );
 }
