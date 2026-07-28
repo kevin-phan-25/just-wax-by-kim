25541a7e3b5f3df6276594d9b1f9b19d3de8c91a
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 08:02:16 -0400

    Update ContactHours.tsx

--- features/contact/ContactHours.tsx
+++ features/contact/ContactHours.tsx
@@ -2,14 +2,14 @@
  * -----------------------------------------------------------------------------
  * File: ContactHours.tsx
  *
- * Created: July 27, 2026
- *
  * Description:
- * Studio operating hours.
+ * Studio operating hours display.
  *
  * Changes:
- * - v1.0.3
- *   - Added business hours display.
+ * - July 28, 2026
+ *   - Updated luxury styling
+ *   - Unified typography/colors
+ *   - Removed default gray palette
  *
  * -----------------------------------------------------------------------------
  */
@@ -17,112 +17,109 @@
 
 const HOURS = [
 
+  {
+    day: "Monday - Friday",
+    time: "9:00 AM - 6:00 PM"
+  },
 
-{
-day:"Monday - Friday",
-time:"9:00 AM - 6:00 PM"
-},
+  {
+    day: "Saturday",
+    time: "9:00 AM - 3:00 PM"
+  },
 
-
-{
-day:"Saturday",
-time:"9:00 AM - 3:00 PM"
-},
-
-
-{
-day:"Sunday",
-time:"Closed"
-}
-
-
+  {
+    day: "Sunday",
+    time: "Closed"
+  }
 
 ];
 
 
 
-export default function ContactHours(){
+export default function ContactHours() {
 
 
-return (
+  return (
+
+    <div>
 
 
-<div>
+      <h3
 
+        className="
+          font-serif
+          text-xl
+          text-brand-espresso
+        "
 
-<h3
+      >
 
-className="
-font-serif
-text-xl
-"
+        Studio Hours
 
->
-
-Studio Hours
-
-</h3>
-
-
-
-<div
-
-className="
-mt-4
-space-y-3
-"
-
->
-
-
-{
-
-HOURS.map(item=>(
-
-
-<div
-
-key={item.day}
-
-className="
-flex
-justify-between
-text-gray-600
-"
-
->
-
-
-<span>
-
-{item.day}
-
-</span>
-
-
-<span>
-
-{item.time}
-
-</span>
-
-
-</div>
-
-
-))
-
-}
+      </h3>
 
 
 
-</div>
+
+      <div
+
+        className="
+          mt-5
+          space-y-4
+        "
+
+      >
+
+        {
+          HOURS.map((item) => (
+
+            <div
+
+              key={item.day}
+
+              className="
+                flex
+                justify-between
+                text-sm
+                text-brand-taupe
+              "
+
+            >
+
+              <span>
+
+                {item.day}
+
+              </span>
 
 
-</div>
+
+              <span
+
+                className="
+                  font-medium
+                  text-brand-espresso
+                "
+
+              >
+
+                {item.time}
+
+              </span>
 
 
-);
+            </div>
+
+          ))
+        }
+
+
+      </div>
+
+
+    </div>
+
+  );
 
 
 }
