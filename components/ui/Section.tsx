fbe936d92c592341d72156b318cfd525755268bb
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 05:12:12 -0400

    Update Section.tsx

--- components/ui/Section.tsx
+++ components/ui/Section.tsx
@@ -1,60 +1,35 @@
 /**
  * -----------------------------------------------------------------------------
- * File:
- * Section.tsx
+ * File: Section.tsx
  *
  * Description:
  * Luxury page section wrapper.
  *
+ * Changes:
+ * - July 28, 2026
+ *   - Increased vertical padding for more breathing room
+ *   - More elegant, less cramped spacing
+ *
  * -----------------------------------------------------------------------------
  */
-
-
 export default function Section({
-
-children,
-
-className=""
-
-}:{
-
-children:React.ReactNode;
-
-className?:string;
-
-}){
-
-
-return (
-
-<section
-
-className={`
-py-24
-px-6
-${className}
-`}
-
->
-
-
-<div
-
-className="
-max-w-7xl
-mx-auto
-"
-
->
-
-{children}
-
-</div>
-
-
-</section>
-
-);
-
-
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <section
+      className={`
+        py-20 md:py-28 lg:py-32
+        px-6
+        ${className}
+      `}
+    >
+      <div className="max-w-7xl mx-auto">
+        {children}
+      </div>
+    </section>
+  );
 }
