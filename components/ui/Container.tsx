4a3d78f9a173c6c4c730310ad33ee48a31d89307
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 05:12:27 -0400

    Adjust padding in Container component
    Updated the Container component to include slightly more generous horizontal padding.

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -8,53 +8,27 @@
  * Global content width wrapper.
  *
  * Changes:
- * - July 27, 2026
- *   - Initial creation.
+ * - July 28, 2026
+ *   - Slightly more generous horizontal padding
  *
  * -----------------------------------------------------------------------------
  */
-
-
-import {
-  cn,
-} from "@/lib/cn";
-
+import { cn } from "@/lib/cn";
 
 interface Props {
-
-children:
-React.ReactNode;
-
-className?:
-string;
-
+  children: React.ReactNode;
+  className?: string;
 }
 
-
-export default function Container({
-
-children,
-
-className,
-
-}: Props) {
-
-
-return (
-
-<div
-
-className={cn(
-"mx-auto w-full max-w-7xl px-6 md:px-10 lg:px-16",
-className
-)}
-
->
-
-{children}
-
-</div>
-
-);
-
+export default function Container({ children, className }: Props) {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-7xl px-6 md:px-10 lg:px-16 xl:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 }
