e1d98cc3b0d73c9687239a85f69e3f13097b023b
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 06:34:43 -0400

    Remove logo and update styling in HeroContent

--- features/hero/HeroContent.tsx
+++ features/hero/HeroContent.tsx
@@ -34,38 +34,3 @@ export default function HeroContent() {
       >
         Luxury Wax Studio
       </p>
-
-      {/* Main Headline */}
-      <h1
-        className="
-          mt-5
-          font-serif
-          text-4xl
-          md:text-6xl
-          lg:text-7xl
-          leading-[1.1]
-          text-brand-espresso
-        "
-      >
-        More Than
-        <br />
-        Just A Wax
-      </h1>
-
-      {/* Supporting line */}
-      <p
-        className="
-          mt-5
-          max-w-md
-          text-sm
-          md:text-base
-          leading-relaxed
-          text-brand-taupe
-        "
-      >
-        A personalized waxing experience designed around comfort,
-        confidence, and you.
-      </p>
-    </div>
-  );
-}
