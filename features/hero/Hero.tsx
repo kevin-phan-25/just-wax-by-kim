9829b91e91a024a53a7f15e3c4926862022c8731
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 06:30:14 -0400

    Update Hero.tsx

--- features/hero/Hero.tsx
+++ features/hero/Hero.tsx
@@ -1,6 +1,7 @@
 /**
  * -----------------------------------------------------------------------------
- * File: Hero.tsx
+ * File:
+ * Hero.tsx
  *
  * Description:
  * Luxury hero experience.
@@ -9,10 +10,12 @@
  * - July 28, 2026
  *   - Removed logo/image from hero
  *   - Rebuilt around the brand quote for a stronger first impression
- *   - Cleaner, more elegant entry experience
+ *   - Reduced top spacing between navbar and hero
+ *   - Improved luxury spacing balance
  *
  * -----------------------------------------------------------------------------
  */
+
 import HeroContent from "./HeroContent";
 import HeroButtons from "./HeroButtons";
 
@@ -21,20 +24,38 @@ export default function Hero() {
     <section
       className="
         relative
-        min-h-[78vh]
-        md:min-h-[85vh]
+        min-h-[72vh]
+        md:min-h-[78vh]
         flex
         items-center
         justify-center
         px-6
-        pt-28
-        pb-20
+        pt-16
+        pb-16
       "
     >
       {/* Soft background glow */}
-      <div className="absolute inset-0 bg-gradient-to-b from-brand-cream via-[#FDF8F7] to-brand-rose/20" />
+      <div
+        className="
+          absolute
+          inset-0
+          bg-gradient-to-b
+          from-brand-cream
+          via-[#FDF8F7]
+          to-brand-rose/20
+        "
+      />
 
-      <div className="relative z-10 w-full max-w-4xl mx-auto text-center">
+      <div
+        className="
+          relative
+          z-10
+          w-full
+          max-w-4xl
+          mx-auto
+          text-center
+        "
+      >
         <HeroContent />
         <HeroButtons />
       </div>
