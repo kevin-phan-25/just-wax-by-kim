d4ef1e6c0448a51d202367188405f0ec9c797793
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 05:20:42 -0400

    Update ServiceGrid.tsx

--- features/services/ServiceGrid.tsx
+++ features/services/ServiceGrid.tsx
@@ -5,63 +5,28 @@
  * Description:
  * Service card collection.
  *
+ * Changes:
+ * - July 28, 2026
+ *   - Cleaner gap + centered grid
+ *
  * -----------------------------------------------------------------------------
  */
-
-
 import ServiceCard from "./ServiceCard";
-
 import { services } from "./services.data";
 
-
-
-export default function ServiceGrid(){
-
-
-return (
-
-
-<div
-
-
-className="
-grid
-gap-8
-md:grid-cols-2
-lg:grid-cols-4
-"
-
-
->
-
-
-{
-
-services.map((service)=>(
-
-
-<ServiceCard
-
-
-key={service.id}
-
-
-service={service}
-
-
-/>
-
-
-))
-
-
-}
-
-
-</div>
-
-
-);
-
-
+export default function ServiceGrid() {
+  return (
+    <div
+      className="
+        grid
+        gap-6 md:gap-8
+        sm:grid-cols-2
+        lg:grid-cols-4
+      "
+    >
+      {services.map((service) => (
+        <ServiceCard key={service.id} service={service} />
+      ))}
+    </div>
+  );
 }
