729f9ed0fc50ad3bb2bcea46c880e14eba8bad04
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-28 07:10:34 -0400

    Update Hero.tsx

--- features/hero/Hero.tsx
+++ features/hero/Hero.tsx
@@ -5,8 +5,9 @@
  *
  * Changes:
  * - July 28, 2026
- *   - Added spacing below navbar
- *   - Added breathing room before Mission section
+ *   - Fixed navbar-to-hero spacing
+ *   - Fixed hero-to-mission spacing
+ *   - Unified background color
  *
  * -----------------------------------------------------------------------------
  */
@@ -19,26 +20,15 @@ export default function Hero() {
     <section
       className="
         relative
-        min-h-0
-        flex
-        items-start
-        justify-center
         px-6
-        pt-8
-        pb-16
+        pt-24
+        pb-20
+        bg-gradient-to-b
+        from-brand-cream
+        via-[#FDF8F7]
+        to-brand-rose/20
       "
     >
-      {/* Soft background glow */}
-      <div
-        className="
-          absolute
-          inset-0
-          bg-gradient-to-b
-          from-brand-cream
-          via-[#FDF8F7]
-          to-brand-rose/20
-        "
-      />
 
       <div
         className="
@@ -53,6 +43,7 @@ export default function Hero() {
         <HeroContent />
         <HeroButtons />
       </div>
+
     </section>
   );
 }
