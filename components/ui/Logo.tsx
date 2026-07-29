84e6a059c5e9eddc0bef89dfb5dec21a009229a2
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-29 11:07:06 -0400

    Update Logo.tsx

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -8,7 +8,7 @@
  * Brand logo for Just Wax by Kim.
  *
  * Changes (July 29, 2026):
- * • Sized to fit the reduced navbar (104px / 124px) without clipping
+ * • Logo height set to ~1.75 inches (168px) inside the ~2 inch navbar
  * -----------------------------------------------------------------------------
  */
 import Image from "next/image";
@@ -29,12 +29,12 @@ export function Logo({ className = "", priority = false }: LogoProps) {
       <Image
         src="/logo/just-wax-by-kim.png"
         alt="Just Wax by Kim"
-        width={220}
-        height={88}
+        width={360}
+        height={168}
         priority={priority}
         className="
-          h-[72px] md:h-[88px] w-auto
-          max-h-[88px]
+          h-[168px] w-auto
+          max-h-[168px]
           object-contain object-left
           transition-opacity duration-300
           group-hover:opacity-80
