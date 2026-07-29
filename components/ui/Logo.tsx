609b62a85d4ca53a9a6fc8feb937b4b10e18bb33
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-29 11:43:16 -0400

    Update Logo.tsx

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -31,11 +31,11 @@ export function Logo({ className = "", priority = false }: LogoProps) {
         src="/logo/just-wax-by-kim.png"
         alt="Just Wax by Kim"
         width={320}
-        height={150}
+        height={200}
         priority={priority}
         className="
-          h-[150px] w-auto
-          max-h-[150px]
+          h-[200px] w-auto
+          max-h-[200px]
           object-contain object-left
           transition-opacity duration-300
           group-hover:opacity-80
