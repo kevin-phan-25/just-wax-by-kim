521c4ec78ebca9d5a78d5793e71c01d4d9cebb88
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-30 15:05:56 -0400

    Update TestimonialCard.tsx

--- features/testimonials/TestimonialCard.tsx
+++ features/testimonials/TestimonialCard.tsx
@@ -2,142 +2,109 @@
  * -----------------------------------------------------------------------------
  * File: TestimonialCard.tsx
  *
- * Created: July 27, 2026
- *
  * Description:
- * Individual testimonial card.
+ * Luxury testimonial presentation card.
  *
  * Changes:
- * - July 27, 2026
- *   - Created luxury review card.
- *
+ * - Editorial quote styling
+ * - Softer borders
+ * - Improved spacing
+ * - Premium typography
  * -----------------------------------------------------------------------------
  */
 
-
-import type {
-
-Testimonial
-
-}
-
-from "./testimonial.types";
-
-
+import type { Testimonial } from "./testimonial.types";
 
 interface Props {
-
-testimonial: Testimonial;
-
+  testimonial: Testimonial;
 }
-
-
 
 export default function TestimonialCard({
+  testimonial,
+}: Props) {
+  return (
+    <article
+      className="
+        relative
+        flex
+        flex-col
+        items-center
+        text-center
+        rounded-[32px]
+        border
+        border-[#E8DDD8]
+        bg-white
+        px-8
+        py-10
+        transition
+        duration-500
+        hover:-translate-y-2
+        hover:shadow-[0_25px_70px_rgba(59,42,38,.08)]
+      "
+    >
 
-testimonial
-
-}: Props){
-
-
-return (
-
-
-<div
-
-className="
-rounded-3xl
-border
-border-brand-border
-bg-brand-white
-p-8
-"
-
->
-
-
-
-<div
-
-className="
-flex
-gap-1
-"
-
->
-
-
-{
-
-Array.from({
-
-length:testimonial.rating
-
-}).map((_,index)=>(
-
-
-<span
-
-key={index}
-
-className="
-text-brand-gold
-"
-
->
-
-★
-
-</span>
+      {/* Stars */}
+      <div
+        className="
+          flex
+          gap-1
+          text-[#D8B4A0]
+        "
+      >
+        {Array.from({
+          length: testimonial.rating,
+        }).map((_, index) => (
+          <span
+            key={index}
+            className="
+              text-lg
+            "
+          >
+            ★
+          </span>
+        ))}
+      </div>
 
 
-))
+      {/* Quote */}
+      <p
+        className="
+          mt-8
+          font-serif
+          text-xl
+          leading-relaxed
+          text-[#6F5A50]
+        "
+      >
+        "{testimonial.quote}"
+      </p>
 
 
-}
+      {/* Divider */}
+      <div
+        className="
+          mt-8
+          h-px
+          w-12
+          bg-[#D8B4A0]
+        "
+      />
 
 
-</div>
+      {/* Name */}
+      <p
+        className="
+          mt-6
+          uppercase
+          tracking-[0.25em]
+          text-sm
+          font-semibold
+          text-[#3B2A26]
+        "
+      >
+        {testimonial.name}
+      </p>
 
-
-
-
-<p
-
-className="
-mt-6
-text-brand-taupe
-leading-relaxed
-"
-
->
-
-"{testimonial.quote}"
-
-</p>
-
-
-
-
-<h3
-
-className="
-mt-6
-font-semibold
-"
-
->
-
-{testimonial.name}
-
-</h3>
-
-
-
-</div>
-
-
-);
-
-
+    </article>
+  );
 }
