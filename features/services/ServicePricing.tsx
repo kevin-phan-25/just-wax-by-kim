12a8e0994efbc4cc2a8229ae7a465f67606721af
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-30 11:20:20 -0400

    Update ServicePricing.tsx

--- features/services/ServicePricing.tsx
+++ features/services/ServicePricing.tsx
@@ -1,19 +1,41 @@
+/**
+ * -----------------------------------------------------------------------------
+ * File:
+ * ServicePricing.tsx
+ *
+ * Description:
+ * Pricing disclaimer.
+ * -----------------------------------------------------------------------------
+ */
+
+
 export default function ServicePricing(){
 
-return(
+
+return (
 
 <p
+
 className="
-mt-10
-text-center
-text-sm
-text-[#8C7468]
+  mt-12
+
+  text-center
+
+  text-sm
+
+  text-[#8C7468]
 "
+
 >
+
 Pricing may vary based on customization.
+<br />
 Consultation available before your appointment.
+
 </p>
 
+
 );
+
 
 }
