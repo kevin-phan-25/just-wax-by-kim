02228decbe7523b8baaa3de0b9dda6d74c29764d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-30 12:41:13 -0400

    Refactor ServiceGrid component for cleaner code

--- features/services/ServiceGrid.tsx
+++ features/services/ServiceGrid.tsx
@@ -1,66 +1,30 @@
-/**
- * -----------------------------------------------------------------------------
- * File:
- * features/services/ServiceGrid.tsx
- * -----------------------------------------------------------------------------
- */
-
 import ServiceCard from "./ServiceCard";
-
-import type {
-  Service,
-} from "./services.types";
-
+import type { Service } from "./services.types";
 
 interface ServiceGridProps {
-
   services: Service[];
-
 }
-
-
 
 export default function ServiceGrid({
-
   services,
-
-}: ServiceGridProps){
-
-
-return (
-
-<div
-
-className="
-grid
-w-full
-grid-cols-1
-md:grid-cols-2
-xl:grid-cols-4
-gap-8
-"
-
->
-
-
-{
-services.map((service)=>(
-
-<ServiceCard
-
-key={service.id}
-
-service={service}
-
-/>
-
-))
-
-}
-
-
-</div>
-
-);
-
+}: ServiceGridProps) {
+  return (
+    <div
+      className="
+        grid
+        w-full
+        grid-cols-1
+        md:grid-cols-2
+        xl:grid-cols-4
+        gap-8
+      "
+    >
+      {services.map((service) => (
+        <ServiceCard
+          key={service.id}
+          service={service}
+        />
+      ))}
+    </div>
+  );
 }
