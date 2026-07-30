a57af63f1614425b3417f036c822012302d6e54f
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-29 22:42:28 -0400

    Update ServiceCategory.tsx

--- features/services/ServiceCategory.tsx
+++ features/services/ServiceCategory.tsx
@@ -2,63 +2,65 @@
  * -----------------------------------------------------------------------------
  * File: ServiceCategory.tsx
  *
- * Created: July 27, 2026
+ * Created:
+ * July 27, 2026
  *
  * Description:
  * Service category heading.
  *
  * Changes:
- * - July 27, 2026
- *   - Added category presentation.
+ *
+ * July 29, 2026
+ *
+ * - Unified with luxury theme
+ * - Reduced visual weight
  *
  * -----------------------------------------------------------------------------
  */
 
-
 import type {
-ServiceCategory
+  ServiceCategory,
 } from "./services.types";
 
 
 interface Props {
-
-category: ServiceCategory;
-
+  category: ServiceCategory;
 }
 
 
-
 export default function ServiceCategory({
-category
-}:Props){
+  category,
+}: Props) {
+
+  return (
+    <div
+      className="
+        text-center
+      "
+    >
+
+      <h3
+        className="
+          font-medium
+          tracking-wide
+          text-brand-espresso
+        "
+      >
+        {category.title}
+      </h3>
 
 
-return (
-
-<div>
-
-<h3 className="
-text-2xl
-font-medium
-">
-
-{category.title}
-
-</h3>
+      <p
+        className="
+          mt-3
+          leading-relaxed
+          text-brand-taupe
+        "
+      >
+        {category.description}
+      </p>
 
 
-<p className="
-mt-3
-text-brand-taupe
-">
-
-{category.description}
-
-</p>
-
-</div>
-
-);
-
-
+    </div>
+  );
 }
