1a3fd857114d53f2fd0a41452dd9fc1f138c8dd7
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-30 12:48:59 -0400

    Update ServicePricingSection.tsx

--- features/services/ServicePricingSection.tsx
+++ features/services/ServicePricingSection.tsx
@@ -8,7 +8,9 @@
  *
  * Changes:
  *
- * - Removed category hero image
+ * - Added category hero image support
+ * - Reduced hero image height
+ * - Matched hero spacing rhythm
  * - Centered service presentation
  * - Removed fixed width restrictions
  * - Unified luxury spacing
@@ -16,11 +18,13 @@
  * -----------------------------------------------------------------------------
  */
 
+import Image from "next/image";
 import ServiceGrid from "./ServiceGrid";
 import type { Service } from "./services.types";
 
 interface ServicePricingSectionProps {
   id: string;
+  image: string;
   title: string;
   description: string;
   services: Service[];
@@ -28,6 +32,7 @@ interface ServicePricingSectionProps {
 
 export default function ServicePricingSection({
   id,
+  image,
   title,
   description,
   services,
@@ -36,11 +41,71 @@ export default function ServicePricingSection({
     <section
       id={id}
       className="
+        relative
+        overflow-hidden
         w-full
-        px-6
         pb-24
       "
     >
+
+      {/* Navbar transition spacer */}
+      <div
+        className="
+          h-[216px]
+        "
+      />
+
+
+      {/* CATEGORY HERO IMAGE */}
+      <div
+        className="
+          relative
+          mx-auto
+          w-full
+          h-[220px]
+          overflow-hidden
+          rounded-[2px]
+          mb-16
+        "
+      >
+        <Image
+          src={image}
+          alt={title}
+          fill
+          sizes="100vw"
+          priority
+          className="
+            object-cover
+            object-center
+          "
+        />
+
+        {/* Luxury Overlay */}
+        <div
+          aria-hidden
+          className="
+            absolute
+            inset-0
+            bg-gradient-to-r
+            from-[#2D211D]/25
+            via-transparent
+            to-[#2D211D]/10
+          "
+        />
+
+        <div
+          aria-hidden
+          className="
+            absolute
+            inset-0
+            bg-gradient-to-t
+            from-[#3B2A26]/20
+            via-transparent
+            to-transparent
+          "
+        />
+      </div>
+
 
       {/* HEADER */}
       <div
@@ -84,6 +149,21 @@ export default function ServicePricingSection({
       >
         <ServiceGrid services={services} />
       </div>
+
+
+      {/* Bottom divider */}
+      <div
+        aria-hidden
+        className="
+          absolute
+          bottom-0
+          left-1/2
+          -translate-x-1/2
+          w-[min(92%,1200px)]
+          h-px
+          bg-[#E8DDD8]
+        "
+      />
 
     </section>
   );
