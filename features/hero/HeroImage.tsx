92e9a05aef8b2a9dcecd5804388af28fbbe28579
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-29 21:18:16 -0400

    Update HeroImage.tsx

--- features/hero/HeroImage.tsx
+++ features/hero/HeroImage.tsx
@@ -17,42 +17,26 @@ export function HeroImage() {
         alt="Just Wax by Kim"
         fill
         priority
+        sizes="100vw"
         className="object-cover object-center"
       />
 
       {/* Luxury Gradient Overlay */}
       <div
         aria-hidden
-        className="
-          absolute inset-0
-          bg-gradient-to-r
-          from-[#2D211D]/25
-          via-transparent
-          to-[#2D211D]/10
-        "
+        className="absolute inset-0 bg-gradient-to-r from-[#2D211D]/25 via-transparent to-[#2D211D]/10"
       />
 
       {/* Soft Bottom Fade */}
       <div
         aria-hidden
-        className="
-          absolute inset-0
-          bg-gradient-to-t
-          from-[#3B2A26]/12
-          via-transparent
-          to-transparent
-        "
+        className="absolute inset-0 bg-gradient-to-t from-[#3B2A26]/12 via-transparent to-transparent"
       />
 
       {/* Decorative Accent */}
       <div
         aria-hidden
-        className="
-          absolute bottom-8 left-1/2
-          -translate-x-1/2
-          w-20 h-px
-          bg-[#D4A9B6]
-        "
+        className="absolute bottom-8 left-1/2 -translate-x-1/2 w-20 h-px bg-[#D4A9B6]"
       />
     </div>
   );
