db38f5d74b3c46d265cd7019eff412fcd39e6642
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-30 21:01:56 -0400

    Update instagram.types.ts

--- features/instagram/instagram.types.ts
+++ features/instagram/instagram.types.ts
@@ -1,26 +1,6 @@
-/**
- * -----------------------------------------------------------------------------
- * File: instagram.types.ts
- *
- * Created: July 27, 2026
- *
- * Description:
- * Instagram post types.
- *
- * -----------------------------------------------------------------------------
- */
-
-
-export interface InstagramPost {
-
-
-id:string;
-
-
-image:string;
-
-
-alt:string;
-
-
-}
+export type InstagramPost = {
+  id: string;
+  image: string;
+  alt: string;
+  href?: string;
+};
