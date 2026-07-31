2fdc1197d3767648a7d4c76291cedc4b4846618c
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-07-31 10:35:26 -0400

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,5 @@
 /**
- * -----------------------------------------------------------------------------
+ * --------------------------------------------------------------------------
  * File:
  * app/gallery/page.tsx
  *
@@ -8,62 +8,31 @@
  *
  * Changes:
  * • Moved Gallery away from homepage
- * • Matches Services page architecture
- * • Added navbar spacing
- * • Full editorial presentation
- * -----------------------------------------------------------------------------
- */
-
-import Gallery from "@/features/gallery";
-
-export default function GalleryPage() {
-  return (
-    <main
-      className="
-        w-full
-        px-6
-        pt-[168px]
-        pb-24
-      "
-    >
-      <Gallery />
-    </main>
-  );
-}
-[kkphan@z11devrm419 gallery]$ vim ../../app/page.tsx 
-[kkphan@z11devrm419 gallery]$ vim ../../app/gallery/page.tsx 
-[kkphan@z11devrm419 gallery]$ vim ../../constants/navigation.ts 
-[kkphan@z11devrm419 gallery]$ vim ../../components/layout/MobileMenu.tsx 
-[kkphan@z11devrm419 gallery]$ cat ../../app/gallery/page.tsx 
-/**
- * -----------------------------------------------------------------------------
- * File:
- * app/gallery/page.tsx
- *
- * Description:
- * Standalone luxury gallery page.
- *
- * Changes:
- * • Gallery moved from homepage
  * • Added dedicated route
- * • Matches Services page spacing
- * -----------------------------------------------------------------------------
+ * • Full-page editorial presentation
+ *
+ * --------------------------------------------------------------------------
  */
+
 
 import Gallery from "@/features/gallery";
 
+
 export default function GalleryPage() {
+
+
   return (
+
     <main
       className="
-        w-full
-        px-6
         pt-[168px]
-        pb-24
       "
     >
-      <Gallery />
-    </main>
-  );
-}
 
+      <Gallery />
+
+    </main>
+
+  );
+
+}
