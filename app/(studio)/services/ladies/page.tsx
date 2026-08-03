d12b217619e2983ebd9a91115604fd9dcbc62bac
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-08-03 09:13:32 -0400

    Update page.tsx

--- app/(studio)/services/ladies/page.tsx
+++ app/(studio)/services/ladies/page.tsx
@@ -7,6 +7,11 @@
  * Description:
  * Ladies waxing pricing page.
  *
+ * Updates:
+ * • Added centered ladies hero image
+ * • Responsive image sizing
+ * • Luxury rounded presentation
+ *
  * ---
  *
  */
@@ -18,11 +23,71 @@ import {
   ladiesServices,
 } from "@/features/services/data/ladies.data";
 
-
 export default function LadiesServicesPage() {
 
   return (
     <>
+
+      {/* LADIES HERO IMAGE */}
+      <section
+        className="
+          w-full
+
+          bg-[#FBF7F4]
+
+          pt-8
+
+          sm:pt-10
+
+          md:pt-14
+
+          px-5
+
+          sm:px-8
+
+          md:px-10
+        "
+      >
+
+        <div
+          className="
+            mx-auto
+
+            flex
+
+            justify-center
+          "
+        >
+
+          <img
+            src="/images/services/ladies_on_page.jpg"
+            alt="Ladies luxury waxing services"
+            className="
+              w-[85%]
+
+              sm:w-[70%]
+
+              md:w-[55%]
+
+              lg:w-[45%]
+
+              max-w-[650px]
+
+              h-auto
+
+              rounded-[32px]
+
+              object-contain
+            "
+          />
+
+        </div>
+
+      </section>
+
+
+
+      {/* PRICING */}
       <ServicePricingPage
         id="ladies-pricing"
 
@@ -36,7 +101,11 @@ export default function LadiesServicesPage() {
         services={ladiesServices}
       />
 
+
+
+      {/* BOOKING */}
       <InclusiveBooking />
+
     </>
   );
 
