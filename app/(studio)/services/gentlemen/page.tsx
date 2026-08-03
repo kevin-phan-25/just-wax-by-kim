819431e85c82826c91b6be1bfc1f2b51b981fb02
kevin-phan-25 <zerotoitsuccess@gmail.com>
2026-08-03 09:14:28 -0400

    Update page.tsx

--- app/(studio)/services/gentlemen/page.tsx
+++ app/(studio)/services/gentlemen/page.tsx
@@ -7,6 +7,11 @@
  * Description:
  * Gentlemen waxing pricing page.
  *
+ * Updates:
+ * • Added centered gentlemen hero image
+ * • Responsive image sizing
+ * • Luxury rounded presentation
+ *
  * ---
  *
  */
@@ -18,11 +23,71 @@ import {
   gentlemenServices,
 } from "@/features/services/data/gentlemen.data";
 
-
 export default function GentlemenServicesPage() {
 
   return (
     <>
+
+      {/* GENTLEMEN HERO IMAGE */}
+      <section
+        className="
+          w-full
+
+          bg-[#FBF7F4]
+
+          pt-8
+
+          sm:pt-10
+
+          md:pt-14
+
+          px-5
+
+          sm:px-8
+
+          md:px-10
+        "
+      >
+
+        <div
+          className="
+            mx-auto
+
+            flex
+
+            justify-center
+          "
+        >
+
+          <img
+            src="/images/services/gentlemen_on_page.jpg"
+            alt="Gentlemen luxury waxing services"
+            className="
+              w-[85%]
+
+              sm:w-[70%]
+
+              md:w-[55%]
+
+              lg:w-[45%]
+
+              max-w-[650px]
+
+              h-auto
+
+              rounded-[32px]
+
+              object-contain
+            "
+          />
+
+        </div>
+
+      </section>
+
+
+
+      {/* PRICING */}
       <ServicePricingPage
         id="gentlemen-pricing"
 
@@ -36,8 +101,13 @@ export default function GentlemenServicesPage() {
         services={gentlemenServices}
       />
 
+
+
+      {/* BOOKING */}
       <InclusiveBooking />
+
     </>
+
   );
 
 }
